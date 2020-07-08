bb3468d5942b5ee31af08fa4cc973495d05102d1
ChachyDev <shalomadecoolboy@outlook.com>
2020-07-08 11:19:48 +0100

    useless class begone

--- src/main/kotlin/club/chachy/kbadwords/features/BadWordAction.kt
+++
@@ -1,7 +0,0 @@
-package club.chachy.kbadwords.features
-
-import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.User
-
-class BadWordAction(val user: User, val message: Message, val guild: Guild)
